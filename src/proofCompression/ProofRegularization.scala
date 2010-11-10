f1b2774ea5177c4c15d0dca3ef96ed6ad9886cf1
bruno.wp <bruno.wp@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2010-11-10 15:58:32 +0000

    Implemented the methods "regularize" and "fixProof".
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@37 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/proofCompression/ProofRegularization.scala
+++ src/proofCompression/ProofRegularization.scala
@@ -5,6 +5,82 @@
 
 package proofCompression
 
-object ProofRegularization {
+import proofCompression.ResolutionCalculus._
+import scala.collection.mutable._
 
+object ProofRegularization {
+  val deletedSubProof = Input(L("DELETED",true)::Nil)
+
+  def regularize(proof:ResolutionProof): Unit = {
+    if (proof.isInstanceOf[Resolvent]) {
+      val n = proof.asInstanceOf[Resolvent]
+      if (allChildrenAreVisited(proof)) {
+        if (proof.children.length > 1) {
+          n.left.literalsBelow += (n -> new HashSet[Literal])
+          n.right.literalsBelow += (n -> new HashSet[Literal])
+        }
+        else if (proof.children.length == 0) {
+          n.left.literalsBelow += (n -> (new HashSet[Literal] + n.pivot._2))
+          n.right.literalsBelow += (n -> (new HashSet[Literal] + n.pivot._1))
+        }
+        else {
+          val literalsBelow = n.literalsBelow.get(n.children.head) match {case Some(set) => set; case None => throw new Exception("Literals Below was not initialized properly") }
+          if (!literalsBelow.contains(n.pivot._1) && !literalsBelow.contains(n.pivot._2)) {
+            n.left.literalsBelow += (n -> (literalsBelow.clone + n.pivot._2))
+            n.right.literalsBelow += (n -> (literalsBelow.clone + n.pivot._1))
+          }
+          else if (literalsBelow.contains(n.pivot._1)) {
+            n.left = deletedSubProof
+            n.right.literalsBelow += (n -> literalsBelow)
+          }
+          else { // if (literalsBelow.contains(n.pivot._2))
+            n.right = deletedSubProof
+            n.left.literalsBelow += (n -> literalsBelow)
+          }
+        }
+        regularize(n.left)
+        regularize(n.right)
+      }
+    }
+  }
+
+  def fixProof(proof:ResolutionProof) = fixProofRec(proof, new HashSet[ResolutionProof])
+  private def fixProofRec(proof:ResolutionProof, visitedProofs: HashSet[ResolutionProof]) : Unit = {
+    if (!visitedProofs.contains(proof)) {
+      visitedProofs += proof
+      proof match {
+        case Input(c) => return
+        case Resolvent(left,right) => {
+          val n = proof.asInstanceOf[Resolvent]
+          fixProofRec(left, visitedProofs)
+          fixProofRec(right, visitedProofs)
+          if (left.clause.contains(n.pivot._1) && right.clause.contains(n.pivot._2)) {
+            n.clause = resolve(left.clause, right.clause)
+          }
+          else {
+            val survivingParent : ResolutionProof =
+            if (left == deletedSubProof) right
+            else if (right == deletedSubProof) left
+            else if (left.clause.contains(n.pivot._1) && !right.clause.contains(n.pivot._2)) right
+            else if (!left.clause.contains(n.pivot._1) && right.clause.contains(n.pivot._2)) left
+            else {
+              if (proofLength(left) < proofLength(right)) left
+              else right
+            }
+            for (child <- n.children) {
+              if (child.left == n) child.left = survivingParent
+              else child.right = survivingParent
+            }
+          }
+        }
+      }
+    }
+  }
+
+  def allChildrenAreVisited(proof:ResolutionProof): Boolean = {
+    for (child <- proof.children) {
+      if (!proof.literalsBelow.contains(child)) return false
+    }
+    return true
+  }
 }
