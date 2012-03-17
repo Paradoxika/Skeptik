3b32a0fc842efdfef1481a826a1ad5df3907c5e0
bruno.wp@gmail.com <bruno.wp@gmail.com@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2012-03-17 18:06:57 +0000

    Added method to convert a list into a string of comma separated values.
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@138 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/ResK/utilities/Utilities.scala
+++ src/ResK/utilities/Utilities.scala
@@ -4,3 +4,10 @@ object debug {
   def apply(condition: Boolean, s: Any) = if (condition) println(s)
   def apply(s: Any) = println(s)
 }
+
+object prettyPrinting {
+  def listToCSVString[X](l:List[X]) = l match {
+    case Nil => ""
+    case _ => (l.head.toString /: l.tail)((x,y)=> "" + x + "," + y)
+  }
+}
