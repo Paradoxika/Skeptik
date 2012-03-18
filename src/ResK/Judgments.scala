522ee85413d9918b91948166dd3fed811205bf0a
bruno.wp@gmail.com <bruno.wp@gmail.com@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2012-03-18 20:02:32 +0000

    Added conversion between sets of formulas and sequents.
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@148 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/ResK/Judgments.scala
+++ src/ResK/Judgments.scala
@@ -1,11 +1,12 @@
 package ResK
 
 object judgments {
-  import scala.collection.TraversableOnce
-  import scala.collection.LinearSeq
+  import scala.collection.immutable.{HashSet => ISet}
+  import scala.collection.mutable.Stack
   import ResK.expressions._
   import ResK.logicalConstants._
   import ResK.utilities.prettyPrinting._
+  import ResK.formulas._
   
   abstract class Judgment
   class Sequent(val ant:List[E], val suc:List[E]) extends Judgment {
@@ -21,13 +22,21 @@ object judgments {
     def ==(s:Sequent) = (ant.toSet == s.ant.toSet) || (suc.toSet == s.suc.toSet)
     override def hashCode = 42*ant.toSet.hashCode + suc.toSet.hashCode
     override def toString = listToCSVString(ant) + " :- " + listToCSVString(suc)
+    def toSet: ISet[E] = ISet() ++ ant.map(f => Neg(f)) ++ suc
   }
   object Sequent {
     def apply(ant:List[E], suc:List[E]) = new Sequent(ant,suc)
     def apply(ant:List[E], suc:E) = new Sequent(ant,suc::Nil)
     def apply(ant:E, suc:List[E]) = new Sequent(ant::Nil,suc)
     def apply() = new Sequent(Nil,Nil)
-    def apply(ant:LinearSeq[E], suc:LinearSeq[E]) = new Sequent(ant.toList,suc.toList)
+    def apply(s: ISet[E]) = {
+      val ant = new Stack[E]; val suc = new Stack[E];
+      for (f <- s) f match {
+        case Neg(g) => ant.push(g)
+        case _ => suc.push(f)
+      }
+      new Sequent(ant.toList,suc.toList)
+    } 
   }
 }
 
