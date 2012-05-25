ffd78cca756ae317c1753182fbf8da12f28ad183
bruno.wp@gmail.com <bruno.wp@gmail.com@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2012-05-25 10:00:41 +0000

    Added companion object for GridGainSeq
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@214 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/ResK/gridgain/GridGainCollection.scala
+++ src/ResK/gridgain/GridGainCollection.scala
@@ -7,7 +7,7 @@ import org.gridgain.grid.GridClosureCallMode._
 
 // This collection class provides parallel/distributed methods
 // map and foreach, based on GridGain's mapReduce framework
-class GridGainCollection[+A](val elements: Seq[A]) {
+class GridGainSeq[+A](val elements: Seq[A]) {
     
   def mapReduce[U,V](f: A => U, r: Seq[U] => V): V = {
     scalar { grid$ @<(SPREAD, for (e <- elements) yield (() => f(e)), r) }
@@ -16,4 +16,7 @@ class GridGainCollection[+A](val elements: Seq[A]) {
   def map[U](f: A => U): Seq[U] = mapReduce(f, (s:Seq[U]) => s)
   
   def foreach[U](f: A => U): Unit = map(f)
+}
+object GridGainSeq {
+  def apply[A](elements: Iterable[A]) = new GridGainSeq(elements.toSeq)
 }
