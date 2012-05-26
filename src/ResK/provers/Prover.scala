380ea340e1f2bd3c00937f9d392fdcafac0c84ce
bruno.wp@gmail.com <bruno.wp@gmail.com@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2012-05-26 11:20:50 +0000

    Cleaned SimpleProver.
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@222 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/ResK/provers/Prover.scala
+++ src/ResK/provers/Prover.scala
@@ -21,20 +21,12 @@ abstract class InferenceRule[J <: Judgment, P <: Proof[J,P]] {
 class SimpleProver[J <: Judgment, P <: Proof[J,P]](calculus: Calculus[J,P]) {
   // Simple generic bottom-up proof search
   def prove(goal:J) : Option[P] = {
-    def proveRec(j: J, seen: ISet[J]): Option[P] = {
+    def proveRec(j: J, seen: ISet[J]): Option[P] = { // "seen" keeps track of goals that already occurred below. This allows the detection of cycles and prevents this kind of non-termination.
       val proofs = for (rule <- calculus.par; subGoals <- rule(j).par) yield {
         def proofOf(g: J) = if (seen contains g) None else proveRec(g, seen + j)
         val premises = subGoals.par.map(subGoal => proofOf(subGoal))
         if (premises.seq contains None) None 
-        else {
-          //println("goal: " + j)
-          //println("premises")
-          //premises.map(println(_))
-          val p = rule(premises.map(_.get).seq, j)
-          //println("proof: ")
-          //println(p)
-          p
-        }
+        else rule(premises.map(_.get).seq, j)
       }
       proofs.find(_.isInstanceOf[Some[P]]).getOrElse(None)
     }
@@ -45,39 +37,3 @@ class SimpleProver[J <: Judgment, P <: Proof[J,P]](calculus: Calculus[J,P]) {
       None // ToDo
     }
   }
-
-//class SimpleGridGainProver[J <: Judgment, P <: Proof[J,P]](calculus: Calculus[J,P]) {
-//  // Simple generic bottom-up proof search
-//  def prove(j: J): Option[P] = {
-//    val proofs: Seq[Option[P]] = for (rule <- calculus; subGoals <- rule(j)) yield {
-//      println()
-//      println(j)
-//      println(rule)
-//      println(subGoals)
-//      println()
-//      val premises: Seq[Option[P]] = GridGainSeq(subGoals).map(subGoal => prove(subGoal))
-////      val premises: Seq[Option[P]] = GridGainSeq(for (subGoal <- subGoals) yield (() => (new SimpleProver(calculus)).prove(subGoal))).map(p => p())
-//      println("premises: " + premises)
-//      if (premises contains None) {
-//        println("premises do not contain None")
-//        None 
-//      }
-//      else {
-//        println("premises contain None")
-//        rule(premises.map(_.get), j)
-//      }
-//    }
-//    val proof = proofs.find(_.isInstanceOf[Some[P]]).getOrElse(None)
-//    println()
-//    println("return")
-//    println(j)
-//    println(proof)
-//    println()
-//    proof
-//  }
-//  
-//  // Simple generic top-down proof search
-//  def prove(axioms: Seq[P], target: J): Option[P] = {
-//    None // ToDo
-//  }
-//}
