45e7bf15fd372ca317fe6f74bf7469e004877f73
bruno.wp@gmail.com <bruno.wp@gmail.com@3a17ae4d-f974-9668-ac4c-bcd87307d02c>
2012-05-27 10:33:56 +0000

    Created a new Prover that can handle side-effects during proof search.
    git-svn-id: http://proof-compression.googlecode.com/svn/trunk/ResolutionProofCompression@228 3a17ae4d-f974-9668-ac4c-bcd87307d02c

--- src/ResK/provers/Prover.scala
+++ src/ResK/provers/Prover.scala
@@ -7,15 +7,22 @@ import scala.collection.immutable.{HashSet => ISet}
 
 
 object typeAliases {
+  type SideEffectState = Any
   type Calculus[J <: Judgment, P <: Proof[J,P]] = Seq[InferenceRule[J, P]]
+  type CalculusWithSideEffects[J <: Judgment, P <: Proof[J,P], S] = Seq[InferenceRuleWithSideEffects[J, P, S]]
 }
 
 import typeAliases._
 
 abstract class InferenceRule[J <: Judgment, P <: Proof[J,P]] {
-  def apply(j: J): Seq[Seq[J]] // applies the rule bottom-up: given a conclusion judgment, returns a sequence of possible premise judgments.
   def apply(premises: Seq[P], conclusion: J): Option[P] // applies the rule top-down: given premise proofs, tries to create a proof of the given conclusion.
-  //def apply(premises: Seq[P]) : Seq[P] 
+  def apply(j: J): Seq[Seq[J]] // applies the rule bottom-up: given a conclusion judgment, returns a sequence of possible premise judgments.  
+//def apply(premises: Seq[P]) : Seq[P] 
+}
+
+abstract class InferenceRuleWithSideEffects[J <: Judgment, P <: Proof[J,P], S] {
+  def apply(premises: Seq[P], conclusion: J)(implicit c: S): Option[P] // applies the rule top-down: given premise proofs, tries to create a proof of the given conclusion.
+  def apply(j: J)(implicit c: S): Seq[Seq[(S,J)]] // applies the rule bottom-up: given a conclusion judgment, returns a sequence of possible premise judgments.
 }
 
 class SimpleProver[J <: Judgment, P <: Proof[J,P]](calculus: Calculus[J,P]) {
@@ -33,7 +40,23 @@ class SimpleProver[J <: Judgment, P <: Proof[J,P]](calculus: Calculus[J,P]) {
     proveRec(goal, new ISet[J])
   }
     // Simple generic top-down proof search
-    def prove(axioms: Seq[P], target: J): Option[P] = {
-      None // ToDo
-    }
+  def prove(axioms: Seq[P], target: J): Option[P] = {
+    None // ToDo
   }
+}
+
+class SimpleProverWithSideEffects[J <: Judgment, P <: Proof[J,P], S](calculus: CalculusWithSideEffects[J,P,S]) {
+  // Simple generic bottom-up proof search
+  def prove(goal:J,context:S) : Option[P] = {
+    def proveRec(j: J, seen: ISet[J], c: S): Option[P] = { // "seen" keeps track of goals that already occurred below, in order to detect and prevent cycles.
+      val proofs = for (rule <- calculus.par; subGoals <- rule(j)(c).par) yield {
+        def proofOf(g: J, c: S) = if (seen contains g) None else proveRec(g, seen + j, c)
+        val premises = subGoals.par.map({case (state,subGoal) => proofOf(subGoal,state)})
+        if (premises.seq contains None) None 
+        else rule(premises.map(_.get).seq, j)(c)
+      }
+      proofs.find(_.isInstanceOf[Some[P]]).getOrElse(None)
+    }
+    proveRec(goal, new ISet[J], context: S)
+  }
+}
