59f34f5bf986d3a85bf95e32072a306f61d20885
Ceilican <bruno.wp@gmail.com>
2012-06-15 23:11:23 +0200

    Cleaned Expressions and added some ToDo tasks.

--- src/main/scala/skeptik/expression/Expressions.scala
+++ src/main/scala/skeptik/expression/Expressions.scala
@@ -7,31 +7,34 @@ import skeptik.judgment.Judgment
 abstract class E extends Judgment {
   def t: T
     
+  // ToDo: should think whether this is really needed.
   def copy: E
     
-  def =+=(e:E) = alphaEquals(e)
-  def alphaEquals(e:E) = {
-    def alphaEqualsRec(e1:E,e2:E,map:IMap[Var,Var]): Boolean = (e1,e2) match {
+  //ToDo: should call canEqual
+  //alphaEquals
+  def =+=(e:E) = {
+    def rec(e1:E,e2:E,map:IMap[Var,Var]): Boolean = (e1,e2) match {
       case (v1:Var, v2:Var) => map.getOrElse(v1,v1)==v2
       case (Abs(v1@Var(_,t1),b1),Abs(v2@Var(_,t2),b2)) => {
-        if (v1 == v2) alphaEqualsRec(b1, b2, map)
-        else if (t1 == t2) alphaEqualsRec(b1, b2, map.updated(v1,v2))
+        if (v1 == v2) rec(b1, b2, map)
+        else if (t1 == t2) rec(b1, b2, map.updated(v1,v2))
         else false
       }
-      case (App(f1,a1),App(f2,a2)) => alphaEqualsRec(f1, f2, map) && alphaEqualsRec(a1, a2, map)
+      case (App(f1,a1),App(f2,a2)) => rec(f1, f2, map) && rec(a1, a2, map)
       case _ => false
     }
-    alphaEqualsRec(this, e, new IMap[Var,Var])
+    rec(this, e, new IMap[Var,Var])
   }
   def occursIn(e:E):Boolean = if (this == e) true else e match {
     case v: Var => false
     case App(f,a) => (this occursIn f) || (this occursIn a)
     case Abs(v,g) => (this occursIn v) || (this occursIn g)
   }
+  // ToDo:  substitutions should be moved outside this class. 
+  // They should be programmed as an internal DSL, similar to positions. 
   type Sub = Pair[Var,E]
-  def /(substitutions: List[Sub]) = applySubstitutions(substitutions) 
-  def applySubstitutions(subs: List[Sub]) = {
-    val subsMap = IMap(subs.map(s => ((s._1.name,s._1.t) -> s._2)): _*)
+  def /(substitutions: List[Sub]) =  {
+    val subsMap = IMap(substitutions.map(s => ((s._1.name,s._1.t) -> s._2)): _*)
     def sRec(f:E,boundVars:ISet[(String,T)]):E = f match {
       case App(e1, e2) => App(sRec(e1,boundVars),sRec(e2,boundVars))
       case Abs(Var(n,t),e) => Abs(Var(n,t), sRec(e, boundVars.+((n,t))))
@@ -41,7 +44,6 @@ abstract class E extends Judgment {
     }
     sRec(this, new ISet[(String,T)])
   }
-  def stringRep = toString
 }
 case class Var(val name: String, override val t:T) extends E {
   override def copy = new Var(name,t)
