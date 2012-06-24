f95eaf47029dd8570f463e6c73ad4c866bfd75bd
Ceilican <bruno.wp@gmail.com>
2012-06-24 21:29:08 +0200

    Add size computation for types

--- src/main/scala/skeptik/expression/types.scala
+++ src/main/scala/skeptik/expression/types.scala
@@ -4,9 +4,14 @@ import skeptik.util.unicode._
 
 sealed abstract class T {
   def ->(t:T) = arrow(this,t)
+  def size: Int
 }
-case object i extends T
-case object o extends T
+trait Atomic {
+  def size = 1
+}
+case object i extends T with Atomic
+case object o extends T with Atomic
 final case class arrow(t1:T, t2:T) extends T {
   override def toString = "(" + t1 + unicodeOrElse("\u2192","->") + t2 + ")"
+  def size = t1.size + t2.size + 1
 }
