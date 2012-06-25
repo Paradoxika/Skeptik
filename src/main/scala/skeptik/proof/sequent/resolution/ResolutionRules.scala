c2e10b17b5a7e5878e3746a7b33f9b383ceff0c7
Ceilican <bruno.wp@gmail.com>
2012-06-25 09:54:34 +0200

    Remove unused apply method from R's companion object

--- src/main/scala/skeptik/proof/sequent/resolution/ResolutionRules.scala
+++ src/main/scala/skeptik/proof/sequent/resolution/ResolutionRules.scala
@@ -37,11 +37,6 @@ with NoMainFormula {
 
 object R {
   def apply(leftPremise:SequentProof, rightPremise:SequentProof, auxL:E, auxR:E)(implicit unifiableVariables:Set[Var]) = new R(leftPremise, rightPremise, auxL, auxR)// unifiableVariables)
-  def apply(leftPremise:SequentProof, rightPremise:SequentProof, findL:E=>Boolean, findR:E=>Boolean)(implicit unifiableVariables:Set[Var]) = {
-    new R(leftPremise, rightPremise,
-          leftPremise.conclusion.suc.find(findL).get,  //TODO: (Bruno) Catch Exception
-          rightPremise.conclusion.ant.find(findR).get) //TODO: (Bruno) Catch Exception
-  }
   def apply(leftPremise:SequentProof, rightPremise:SequentProof)(implicit unifiableVariables:Set[Var]) = {
     def isUnifiable(p:(E,E)) = unify(p::Nil)(unifiableVariables) match {
         case None => false
