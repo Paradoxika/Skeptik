9b19c2b3f47b916b1115deb3870287bff9f55c9a
Ceilican <bruno.wp@gmail.com>
2012-07-06 15:55:01 +0200

    Implement argMin function to return the minimum element of a collection

--- src/main/scala/skeptik/util/Utilities.scala
+++ src/main/scala/skeptik/util/Utilities.scala
@@ -12,6 +12,22 @@ object debug {
   }
 }
 
+object argMin {
+  def apply[A](s: Traversable[A], size: A => Int) = {
+    def rec(t: Traversable[A]): (Option[A],Int) = t.toList match {
+      case Nil => (None, Int.MaxValue)
+      case h::tail => {
+        val r @ (bestInTail,min) = rec(tail)
+        val hSize = size(h)
+        if (hSize < min) (Some(h), hSize)
+        else r
+      }
+    }
+    rec(s)._1
+  }
+  
+}
+
 object time {
   import actors.Futures.{awaitAll, future}
   
