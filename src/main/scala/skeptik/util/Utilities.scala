6e8618a794637bd25d54dd427a5f73c17919923a
Ceilican <bruno.wp@gmail.com>
2012-07-06 07:59:40 +0200

    Change time measurement from microseconds to milliseconds

--- src/main/scala/skeptik/util/Utilities.scala
+++ src/main/scala/skeptik/util/Utilities.scala
@@ -15,7 +15,7 @@ object debug {
 object time {
   import actors.Futures.{awaitAll, future}
   
-  def timeout[R](time: Long)(f: => R): Option[R] = awaitAll(time, future {f}).head.asInstanceOf[Option[R]]
+  def timeout[R](time: Long)(f: => R): Option[R] = awaitAll(time, future { f }).head.asInstanceOf[Option[R]]
 
   def timeout[R](time: Long, default: R)(f: => R): R = timeout(time)(f).getOrElse(default)
   
@@ -24,13 +24,13 @@ object time {
     System.gc()
     val now = System.nanoTime
     val result = f
-    val time = (System.nanoTime - now) / 1000 // in microseconds
+    val time = (System.nanoTime.toDouble - now) / 1000000 // in milliseconds
     Timed(result, time)
   }
   def timed[R](repetitions: Int)(f: => R): Timed[R] = {
     val Timed(r,t) = timed { f }
     val averageTime = (for (i <- 1 to repetitions - 1) yield timed(f).time).foldLeft(t)(_+_) / repetitions
-    Timed(r, averageTime.floor)
+    Timed(r, averageTime)
   }
 }
     
