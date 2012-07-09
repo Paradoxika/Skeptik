7853a03e948ef61a60decb1a1d077d128f7460bf
Ceilican <bruno.wp@gmail.com>
2012-07-09 23:51:55 +0200

    Handle missing arguments in the command line in a better way

--- src/main/scala/skeptik/Main.scala
+++ src/main/scala/skeptik/Main.scala
@@ -2,39 +2,42 @@ package skeptik
 
 object Main {
   def main(args: Array[String]): Unit = {
-    try {
-      if (args(0) == "-experiment") {
-        if (args(1) == "--NDc") {
-          skeptik.experiment.proving.ProverExperiment.run(Array());
-        }   
-        else if (args(1) == "--compression") {
-          skeptik.experiment.compression.Experimenter.run(args.drop(2));
-        } 
-      }
-      else if (args(0) == "-prove") {
-        
-      }
-      else if (args(0) == "-compress") {
-        
-      }
+    def help() = print(usageInstructions)
+
+    if (args.length == 0) help()
+    else if (args(0) == "-experiment") {
+      if (args.length == 1) help()
+      else if (args(1) == "--NDc") {
+        skeptik.experiment.proving.ProverExperiment.run(Array());
+      }   
+      else if (args(1) == "--compression") {
+        skeptik.experiment.compression.Experimenter.run(args.drop(2));
+      } 
+      else help()
     }
-    catch {
-      case _ => {
-println(
-"""
-Skeptik Usage Instructions
-==========================
-    
-  -experiment    : runs pre-defined experiment
-    --NDc          : contextual natural deduction experiment
-    --compression  : proof compression experiment
+    else if (args(0) == "-prove") {
+      
+    }
+    else if (args(0) == "-compress") {
+      
+    }
+    else help()
 
-  -prove         : proves theorem
-
-  -compress      : compress proof
-""" 
-)
-      }
-    }  
   }
+    
+  private val usageInstructions = 
+  """
+    
+  Skeptik Usage Instructions
+  ==========================
+      
+    -experiment    : runs pre-defined experiment
+      --NDc          : contextual natural deduction experiment
+      --compression  : proof compression experiment
+  
+    -prove         : proves theorem
+  
+    -compress      : compress proof
+    
+  """   
 }
