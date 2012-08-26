a3e4a7006355d046becb77febeecc35a19c24413
Ceilican <bruno.wp@gmail.com>
2012-08-26 15:54:45 +0200

    Make RichIterable extend AnyVal in order to avoid overhead of implicit conversion

--- src/main/scala/at/logic/skeptik/util/utilities.scala
+++ src/main/scala/at/logic/skeptik/util/utilities.scala
@@ -29,7 +29,7 @@ object argMin {
 }
 
 object rich {
-  class RichIterable[T](c: Iterable[T]) {
+  class RichIterable[T](val c: Iterable[T]) extends AnyVal {
     def contains(e: T) = if (c.isInstanceOf[Set[_]]) c.asInstanceOf[Set[T]].contains(e)
                          else if (c.isInstanceOf[Seq[_]]) c.asInstanceOf[Seq[T]].contains(e)
                          else c.exists(_ == e)
@@ -38,7 +38,10 @@ object rich {
                          else c.filterNot(new RichIterable(that).contains(_))
     def intersect(that: Iterable[T]) = if (c.isInstanceOf[Set[_]]) c.asInstanceOf[Set[T]] intersect that.toSet
                          else if (c.isInstanceOf[Seq[_]]) c.asInstanceOf[Seq[T]] intersect that.toSeq
-                         else c.filterNot(new RichIterable(that).contains(_))
+                         else throw new Exception("not implemented yet") //ToDo: fix this line
+    def union(that: Iterable[T]) = if (c.isInstanceOf[Set[_]]) c.asInstanceOf[Set[T]] union that.toSet
+                         else if (c.isInstanceOf[Seq[_]]) c.asInstanceOf[Seq[T]] union that.toSeq
+                         else c ++ that                        
   }
   object RichIterable {
     implicit def enrichIterable[T](c: Iterable[T]) = new RichIterable(c)
