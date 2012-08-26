e0789a6c6e4011b058a2dee15e03bfc990a3756a
Ceilican <bruno.wp@gmail.com>
2012-08-26 16:07:44 +0200

    Fix Test

--- src/test/scala/at/logic/skeptik/judgment/Sequent.scala
+++ src/test/scala/at/logic/skeptik/judgment/Sequent.scala
@@ -14,7 +14,7 @@ class TestsForSequent extends SpecificationWithJUnit {
 
   val a = Var("a", o)
   val x = Var("x", o)
-  val s = Sequent(List(a,x),List(a,x,a,x)) 
+  val s = immutable.SeqSequent(a,x)(a,x,a,x)
   
   "Sequent" should {
     "give a nicely formated string when toString is called" in {
