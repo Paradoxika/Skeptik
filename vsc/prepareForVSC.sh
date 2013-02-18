fb37295f4c2d0261b2deb8d275baf10443e9141c
Ceilican <bruno.wp@gmail.com>
2013-02-18 20:31:07 -0300

    Remove buildVSC.sbt
    With the new version of SBT, there is no need to have a different "build.sbt" file for SBT. Consequently, "prepareForVSC.sh" was also simplified.

--- vsc/prepareForVSC.sh
+++ vsc/prepareForVSC.sh
@@ -1,5 +1,3 @@
-cp ../build.sbt ../buildNotForVSC.sbt
-cp buildVSC.sbt ../build.sbt
 cd ..
 sbt one-jar
-cp ./target/scala-2.10.0-M7/skeptik_2.10.0-M7-0.2-SNAPSHOT-one-jar.jar vsc/skeptik.jar
+cp ./target/scala-2.10/skeptik_2.10-1.0-one-jar.jar vsc/skeptik.jar
