1e2f4c18e3fe8c59449e7161e556d1abb93c10e5
Ceilican <bruno.wp@gmail.com>
2013-03-05 08:41:43 -0300

    Bump Scala version

--- admin/update-pom.sh
+++ admin/update-pom.sh
@@ -2,7 +2,13 @@
 
 # Updates pom.xml to reflect changes in build.sbt
 
-cd ..
+root_dir=".."
+admin_dir="admin"
+
+scala_version="2.10.1-RC2"
+skeptik_version="1.0"
+
+cd $root_dir
 sbt make-pom 
-cp target/scala-2.10/skeptik_2.10-1.0.pom pom.xml
-cd admin
+cp target/scala-${scala_version}/skeptik_${scala_version}-${skeptik_version}.pom pom.xml
+cd $admin_dir
