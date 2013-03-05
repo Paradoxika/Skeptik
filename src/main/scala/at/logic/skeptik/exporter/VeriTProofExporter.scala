624df6fb81ea592ade0081e310666cd444a3b023
Ceilican <bruno.wp@gmail.com>
2013-03-05 08:46:31 -0300

    Add first draft of proof exporter using VeriT's format

---
+++ src/main/scala/at/logic/skeptik/exporter/VeriTProofExporter.scala
@@ -0,0 +1,45 @@
+package at.logic.skeptik.exporter
+
+import at.logic.skeptik.proof.Proof
+import at.logic.skeptik.proof.sequent.{SequentProofNode => ProofNode}
+import at.logic.skeptik.proof.sequent.lk.{CutIC, Axiom}
+import java.io.FileWriter
+
+
+
+object VeriTProofExporter {
+  def writeProofToFile(proof:Proof[ProofNode], filename: String) = {
+    val writer = new FileWriter(filename)
+    
+    var counter = 0
+    
+    proof foldDown { 
+      (n, premiseResults: Seq[String]) => {
+        n match {
+          case Axiom(clause) => {
+              val name = ".c" + counter
+              counter += 1
+              val line = "(set " + name + " (input :conclusion " + clause + "))\n"
+              writer.write(line, 0, line.length)
+              name
+          }
+          case CutIC(left,right,_,_) => {
+            if (proof.childrenOf(n).length == 1) {
+              "(" + premiseResults(0) + " " + premiseResults(1) + ")"
+            }
+            else {
+              val name = ".c" + counter
+              counter += 1
+              val subproof = "(" + premiseResults(0) + " " + premiseResults(1) + ")"
+              val line = "(set " + name + " (resolution :clauses " + subproof + "))\n"
+              writer.write(line, 0, line.length)
+              name
+            }
+          }
+        }  
+      } 
+    }
+ 
+    writer.close
+  }
+}
