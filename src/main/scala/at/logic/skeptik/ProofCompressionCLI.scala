bd1c3c0aaba2cd4546156e73849847bc9ebc12af
Ceilican <bruno.wp@gmail.com>
2013-03-08 12:47:23 -0300

    Add proof exporter for Skeptik's format

--- src/main/scala/at/logic/skeptik/ProofCompressionCLI.scala
+++ src/main/scala/at/logic/skeptik/ProofCompressionCLI.scala
@@ -2,16 +2,18 @@ package at.logic.skeptik
 
 import at.logic.skeptik.parser.ProofParserVeriT
 import at.logic.skeptik.exporter.ProofExporterVeriT
+import at.logic.skeptik.exporter.ProofExporterSkeptik
 import at.logic.skeptik.algorithm.compressor.Algorithms
 
 object ProofCompressionCLI {
 
   case class Config(input: String = "",
                     algorithm: String = "RPI", 
-                    output: String = "")
-                   
-                      
-  def main(args: Array[String]): Unit = {                     
+                    output: String = "")                
+      
+  def unknownFormat(filename: String) = "Unknown format for " + filename + ". Supported formats are '.smt2' and '.skeptik'"                 
+                    
+  def main(args: Array[String]): Unit = {  
     val parser = new scopt.immutable.OptionParser[Config]("compress", "\n\nSkeptik's Command Line Interface for Proof Compression\n\n") { def options = Seq(
       opt("a", "algorithm", "the algorithm to be used for compressing the proof") { (v: String, c: Config) => c.copy(algorithm = v) },
       opt("o", "output", "<output file>", "file to store the compressed proof") { (v: String, c: Config) => c.copy(output = v) },
@@ -23,7 +25,7 @@ object ProofCompressionCLI {
       println("Reading proof...")
       val proof = ("""\.[^\.]+$""".r findFirstIn config.input) match {
         case Some(".smt2")  => ProofParserVeriT.read(config.input)
-        case _ => throw new Exception("Unknown format for " + config.input)
+        case _ => throw new Exception(unknownFormat(config.input))
       }
       
       println("Compressing proof...")
@@ -31,7 +33,12 @@ object ProofCompressionCLI {
       
       if (config.output != "") {
         println("Writing compressed proof...")
-        ProofExporterVeriT.write(compressedProof, config.output)
+        ("""\.[^\.]+$""".r findFirstIn config.output) match {
+          case Some(".smt2") => ProofExporterVeriT.write(compressedProof, config.output)
+          case Some(".skeptik") => ProofExporterSkeptik.write(compressedProof, config.output)
+          case _ => throw new Exception(unknownFormat(config.output))
+        }
+        
       }
        
       
