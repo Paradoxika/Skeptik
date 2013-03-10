f90a5235f3b69f87deca447a1c554c3b05362773
Ceilican <bruno.wp@gmail.com>
2013-03-10 10:38:11 -0300

    Add more functions to math utility package

--- src/main/scala/at/logic/skeptik/util/math.scala
+++ src/main/scala/at/logic/skeptik/util/math.scala
@@ -1,17 +1,22 @@
 package at.logic.skeptik.util
 
 object math {
-  def argMin[A](s: Traversable[A], size: A => Int) = {
+  def argMax[A](s: Traversable[A], size: A => Int) = genericMinMax(s,size,Int.MinValue, _ > _)._1
+  def argMin[A](s: Traversable[A], size: A => Int) = genericMinMax(s,size,Int.MaxValue, _ < _)._1
+  
+  def max[A](s: Traversable[A], size: A => Int, default: Int = Int.MinValue) = genericMinMax(s,size,default, _ > _)._2
+  def min[A](s: Traversable[A], size: A => Int, default: Int = Int.MaxValue) = genericMinMax(s,size,default, _ < _)._2
+  
+  def genericMinMax[A](s: Traversable[A], size: A => Int, default: Int, compare: (Int,Int) => Boolean ) = {
     def rec(t: Traversable[A]): (Option[A],Int) = t.toList match {
-      case Nil => (None, Int.MaxValue)
+      case Nil => (None, default)
       case h::tail => {
-        val r @ (bestInTail,min) = rec(tail)
+        val r @ (bestInTail,max) = rec(tail)
         val hSize = size(h)
-        if (hSize < min) (Some(h), hSize)
+        if (compare(hSize,max)) (Some(h), hSize)
         else r
       }
     }
-    rec(s)._1
-  }
-  
+    rec(s)
+  }  
 }
