352bdbff838e0cd868b9e2e2f8b7279d930644a3
AFellner <fellner.a@gmail.com>
2013-07-23 17:21:02 +0200

    Implement extended csv format, merge with batch processing of CLI

--- src/main/scala/at/logic/skeptik/ProofCompressionCLI.scala
+++ src/main/scala/at/logic/skeptik/ProofCompressionCLI.scala
@@ -10,6 +10,11 @@ import at.logic.skeptik.util.time._
 import at.logic.skeptik.util.pretty._
 
 import java.io.FileWriter
+import java.io.File
+import java.io.BufferedReader
+import java.io.FileReader
+import scala.io.Source.fromFile
+
 
 object ProofCompressionCLI {
 
@@ -25,19 +30,29 @@ object ProofCompressionCLI {
   def unknownAlgorithm(a: String) = "Algorithm " + a + " is unknown."
   
   def main(args: Array[String]): Unit = {  
-    val parser = new scopt.immutable.OptionParser[Config]("compress","- Skeptik's Command Line Interface for Proof Compression") { def options = Seq(
-      opt("a", "algorithms", "<algorithms>", "comma-separated list of algorithms to be used for compressing the proof - sequential composition of algorithms is denoted by \"(Alg1*Alg2*...*Alg_n)\"") { 
-        (v: String, c: Config) => c.copy(algorithms = v.split(",")) 
-      },
-      opt("o", "outputformat", "<output format>", "proof format to be used for the compressed proofs") { 
-        (v: String, c: Config) => c.copy(outputformat = v) 
-      },
-      flag("csv", "csv", "activates output of proof compression statistics to a csv file") { 
-        (c: Config) => c.copy(csv = true) 
-      },
-      arglist("<input files>", "space-separated filenames of the files containing the proofs to be compressed") { 
-        (v: String, c: Config) => c.copy(inputs = c.inputs ++ Seq(v)) }
-    ) }
+    val parser = new scopt.OptionParser[Config]("compress"){
+      head("- Skeptik's Command Line Interface for Proof Compression") 
+
+      opt[String]('a', "algorithms") action { (v, c) => 
+        c.copy(algorithms = v.split(",")) 
+      } text("comma-separated list of algorithms to be used for compressing the proof - sequential composition of algorithms is denoted by \"(Alg1*Alg2*...*Alg_n)\"")
+      
+      opt[String]('o', "outputformat") action { (v, c) => 
+        c.copy(outputformat = v) 
+      } text("proof format to be used for the compressed proofs")
+      
+      opt[Unit]("csv") action { (_, c) =>
+        c.copy(csv = true) 
+      } text("activates output of proof compression statistics to a csv file")
+      
+      opt[String]('b', "batch") action { (v, c) => 
+        c.copy(inputs = c.inputs ++ fromFile(v).getLines) 
+      } text("file containing one proof filename per line")
+      
+      arg[String]("<file>...") unbounded() optional() action { (v, c) =>
+        c.copy(inputs = c.inputs ++ Seq(v)) 
+      } text("filenames of the proof files")
+    }
     // parser.parse returns Option[C]
     parser.parse(args, Config()) map { config =>
       
@@ -46,12 +61,55 @@ object ProofCompressionCLI {
       // measurement table initialized with its header only
       // rows with data for every input or output proof are added during execution 
       // and the table is displayed to the user at the end
-      var measurementTable: Seq[Seq[Any]] = Seq(Seq("Proof", "Length", "Width", "Height")) 
+      var measurementTable: Seq[Seq[Any]] = Seq(Seq("Proof", "Length", "Core", "Height")) 
+      
+      val csvFile = new File(config.algorithms.mkString(",") + ".csv")
       
       // convenient method for writing compression statistics in a csv file
-      val csvWriter = if (config.csv) Some(new FileWriter(config.algorithms.mkString(",") + ".csv"))
+      val csvWriter = if (config.csv) Some(new FileWriter(csvFile,true))
                       else None
       def writeToCSV(s: String) = for (w <- csvWriter) w.write(s,0,s.length)
+      
+      val algcount = config.algorithms.size
+      var lengths = Array.fill[Int](algcount+1)(0)
+     
+      if (config.csv) {
+        var reader:BufferedReader = null
+        try{
+          reader = new BufferedReader(new FileReader(csvFile))
+          var currLine = reader.readLine()
+          var lastLine = ""
+          
+          while (currLine != null) {
+            lastLine = currLine
+            currLine = reader.readLine()
+          }
+          if (!(lastLine.equals(""))) {
+            val split = lastLine.split(",")
+            lengths(0) = split(4).toInt
+            for (i <- 1 to algcount)
+              lengths(i) = split(3+i*5).toInt
+          }
+        } finally try { reader.close }
+      }
+      
+
+      
+      
+      val writeHeader = (config.csv && csvFile.length == 0)
+      
+      if (writeHeader) writeCSVHeader
+      
+      def writeCSVHeader = {
+        writeToCSV("\t,Original,\t,\t,\t,")
+        config.algorithms.foreach(a => writeToCSV(a+",\t,\t,\t,\t,"))
+        writeToCSV("\n\t,")
+        writeToCSV("\tLength,\tWidth,\tHeight,\tTotal length,")
+        for (i <- 1 to (config.algorithms.size)) writeToCSV("\tLength,\tWidth,\tHeight,\tTotal length,\tCompression Ratio,")
+        writeToCSV("\n")
+      }
+      
+      
       
       for (filename <- config.inputs) {
         // Reading the proof
@@ -78,6 +136,8 @@ object ProofCompressionCLI {
         writeToCSV(proofName + ",\t")
         writeToCSV(mIProof.toSeq.mkString("\t",",\t", ","))
         
+        lengths(0) = lengths(0)+mIProof.toSeq(0)
+        writeToCSV(lengths(0)+",")
         
         
         val writeProof =  {
@@ -88,6 +148,7 @@ object ProofCompressionCLI {
           }
         }
         
+        var alg = 1
         // Compressing the proof
         for (a <- config.algorithms) yield {
           val algorithm = AlgorithmParser.parse(a)
@@ -106,6 +167,10 @@ object ProofCompressionCLI {
           
           // Adding measurements to csv file
           writeToCSV(mOProof.toSeq.mkString("\t",",\t", ","))
+          lengths(alg) = lengths(alg)+mOProof.toSeq(0)
+          writeToCSV(lengths(alg)+",")
+          
+          writeToCSV((100 - Math.round(1000.0*lengths(alg)/lengths(0))/10.0)+"%,")
           
           // Adding measurements to measurement table
           val outputRow = {
@@ -115,6 +180,7 @@ object ProofCompressionCLI {
             Seq(oProofName) ++ ((mOProof.toSeq zip compressions) map {case (o,c) => o + " (" + c + ")"}) 
           }
           measurementTable ++= Seq(outputRow)
+          alg = alg + 1
         }            
          
         writeToCSV("\n")
@@ -132,7 +198,7 @@ object ProofCompressionCLI {
 """ 
   where:           
     Length = number of inferences in the proof
-    Width = number of axioms in the proof
+    Core = number of axioms in the proof
     Height = length of longest path from leaf to root
           
 """)
