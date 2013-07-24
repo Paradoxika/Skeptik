7f3069a4722cfcbc59580d42c27275b346a39e3b
Ceilican <bruno.wp@gmail.com>
2013-07-24 17:35:58 +0400

    Shorten algorithm abbreviations

--- src/main/scala/at/logic/skeptik/algorithm/compressor/package.scala
+++ src/main/scala/at/logic/skeptik/algorithm/compressor/package.scala
@@ -3,32 +3,33 @@ package at.logic.skeptik.algorithm
 import at.logic.skeptik.algorithm.compressor.split._
 
 // Algorithm names should contain only alphanumeric characters
-// otherwise, the command line parser does not work.
+
+// ToDo: the name of an algorithm should be a property defined in the algorithm's class
 
 package object compressor {
   val algorithms = Map(
-    "RPI" -> RecyclePivotsWithIntersection,
-    "LU" -> LowerUnits,
-    "RP" -> RecyclePivots,
-    "RPI3LU" -> IdempotentThreePassLowerUnits,
-    "LUniv" -> LowerUnivalents,
-    "LUnivRPI" -> IdempotentLowerUnivalentsAfterRecyclePivots,
-    "RPI3LUniv" -> LowerUnivalentsBeforeRecyclePivots,
-    "RedRec" -> new ReduceAndReconstruct(5000),
-    "RRlm" -> new RRWithLowerMiddle(5000),
-    "lmA2" -> new LowerMiddleA2(5000),
-    "CottonSplit" -> new CottonSplit(30000),
-    "RandomBoudouSplit" -> new RandomBoudouSplit(30000),
-    "DeterministicBoudouSplit" -> new DeterministicBoudouSplit(30000),
-    "MultiSplit2" -> new TimeoutMultiSplit(2,5000),
-    "MultiSplit3" -> new TimeoutMultiSplit(3,5000),
-    "MultiSplit4" -> new TimeoutMultiSplit(4,5000),
     "DAGify" -> DAGify,
     "ET" -> EliminateTautologies,
+    "RU" -> RecycleUnits,
+    "RP" -> RecyclePivots,
+    "RPI" -> RecyclePivotsWithIntersection,
+    "RPC" -> RecyclePivotsWithConclusionSequent,
+    "LU" -> LowerUnits,
+    "LUniv" -> LowerUnivalents,
+    "RPI3LU" -> IdempotentThreePassLowerUnits,
+    "LUnivRPI" -> IdempotentLowerUnivalentsAfterRecyclePivots,
+    "RPI3LUniv" -> LowerUnivalentsBeforeRecyclePivots,
+    "RR" -> new ReduceAndReconstruct(5000),
+    "RRlm" -> new RRWithLowerMiddle(5000),
+    "lmA2" -> new LowerMiddleA2(5000),
+    "Split" -> new CottonSplit(30000),
+    "RBSplit" -> new RandomBoudouSplit(30000),
+    "DBSplit" -> new DeterministicBoudouSplit(30000),
+    "MSplit2" -> new TimeoutMultiSplit(2,5000),
+    "MSplit3" -> new TimeoutMultiSplit(3,5000),
+    "MSplit4" -> new TimeoutMultiSplit(4,5000),
     "FWS" -> FWS,
     "BWSt" -> BWSt,
-    "BWSm" -> BWSm,
-    "RU" -> RecycleUnits,
-    "RPC" -> RecyclePivotsWithConclusionSequent
+    "BWSm" -> BWSm
   )
 }
