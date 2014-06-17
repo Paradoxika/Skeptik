9a24da444a2fdefb1ce6180ceb93f50c3273a761
AFellner <fellner.a@gmail.com>
2014-06-17 22:18:22 +0200

    Change how equations are added in congruence compressor algorithm

--- src/main/scala/at/logic/skeptik/algorithm/compressor/congruence/CongruenceCompressorNew.scala
+++ src/main/scala/at/logic/skeptik/algorithm/compressor/congruence/CongruenceCompressorNew.scala
@@ -31,74 +31,74 @@ abstract class CongruenceCompressorNew extends (Proof[N] => Proof[N]) with fixNo
     val eqNodesLeft = MMap[EqW,N]()
     
     def traversal(node: N, fromPr: Seq[(N,Boolean)]): (N,Boolean) = {
-      
-      val fixedNode = fixNode(node,fromPr.map(_._1))
-      var theorylemma = 
-        if (fromPr.isEmpty)
-          fixedNode.isInstanceOf[EqAxiom]
-        else fromPr.map(_._2).forall(b => b)
-      
-      val rightEqs = fixedNode.conclusion.suc.filter(EqW.isEq(_)).map(EqW(_))
-      val leftEqs = fixedNode.conclusion.ant.filter(EqW.isEq(_)).map(EqW(_))
-      
-      val singleLeft = fixedNode.conclusion.suc.size == 0 && fixedNode.conclusion.ant.size == 1 && fixedNode.conclusion.ant.forall(EqW.isEq(_))
-      if (singleLeft) {
-        val eq = EqW(fixedNode.conclusion.ant.last)
-        eqNodesLeft += (eq -> node)
-      }
-      
-      if (fixedNode.conclusion.suc.size == 1 && fixedNode.conclusion.ant.size == 0) axiomEqs += (fixedNode.conclusion.suc.last -> fixedNode)
-      
-      if (fixedNode.conclusion.suc.size == 1 && fixedNode.conclusion.suc.forall(EqW.isEq(_))) 
-        resolveWithMap.update(fixedNode.conclusion.suc.last, resolveWithMap.getOrElse(fixedNode.conclusion.suc.last, MSet[N]()) += fixedNode)
-      val resNode = {
-        tried = tried + 1
-        val eqToMap = rightEqs.map(eq => {
-          val con = newCon.addAll(leftEqs).addNode(eq.l).addNode(eq.r).updateLazy
-          con.explain(eq.l,eq.r) match {
-            case Some(path) => {
-              
-              
-              
-              path.toProof match {
-                case Some(proof) => {
-                  val newSize = proof.root.conclusion.ant.size
-                  val oldSize = leftEqs.size
-                  
-//                  println(newSize == proof.root.conclusion.ant.size)
-                  
-                  val line = oldSize + ", " + newSize + ", " + theorylemma + "\n"
-                  output.write(line)
-//                  val fixedProof = Proof(fixedNode)
-//                    if (proof.size > fixedProof.size) {
-//                      val oldProof = Proof(fixedNode)
-//                      println("increasing proof size, length:" + measure(oldProof)("length") + " vs " + measure(proof)("length") +  " trans length:" + measure(oldProof)("transLength") + " vs " + measure(proof)("transLength") + "\n" + Proof(fixedNode) + "\nto\n"+proof)
-//                    }
-//                    println(proof.root.conclusion.ant.size == path.originalEqs.size)
-//                    if (fixedNode.conclusion.size < proof.root.conclusion.size) println("made it bigger: " + fixedNode.conclusion.size +" vs " + proof.root.conclusion.size)
-//                    if (fixedNode.conclusion.size == proof.root.conclusion.size) println("stayed the same")
-//                    if (fixedNode.conclusion.size > proof.root.conclusion.size) println("got smaller: " + fixedNode.conclusion.size +" vs " + proof.root.conclusion.size)
-                  comp = comp + (fixedNode.conclusion.size - proof.root.conclusion.size)
-//                    println("compressing")
-                  proof.root
-                }
-                case None => fixedNode
-              }
-            }
-            case _ => fixedNode
-          }
-        })
+      if (fromPr.isEmpty) (node,node.isInstanceOf[EqAxiom])
+      else {
+        val fixedNode = fixNode(node,fromPr.map(_._1))
+        var theorylemma = fromPr.map(_._2).forall(b => b)
         
-        val x = if (eqToMap.isEmpty) {
-//            println("reach this")
-          fixedNode 
+        val rightEqs = fixedNode.conclusion.suc.filter(EqW.isEq(_)).map(EqW(_))
+        val leftEqs = fixedNode.conclusion.ant.filter(EqW.isEq(_)).map(EqW(_))
+        
+  //      val singleLeft = fixedNode.conclusion.suc.size == 0 && fixedNode.conclusion.ant.size == 1 && fixedNode.conclusion.ant.forall(EqW.isEq(_))
+  //      if (singleLeft) {
+  //        val eq = EqW(fixedNode.conclusion.ant.last)
+  //        eqNodesLeft += (eq -> node)
+  //      }
+        
+        if (fixedNode.conclusion.suc.size == 1 && fixedNode.conclusion.ant.size == 0) axiomEqs += (fixedNode.conclusion.suc.last -> fixedNode)
+        
+        if (fixedNode.conclusion.suc.size == 1 && fixedNode.conclusion.suc.forall(EqW.isEq(_))) 
+          resolveWithMap.update(fixedNode.conclusion.suc.last, resolveWithMap.getOrElse(fixedNode.conclusion.suc.last, MSet[N]()) += fixedNode)
+        val resNode = {
+          tried = tried + 1
+          val con = newCon.addAll(leftEqs)
+          val eqToMap = rightEqs.map(eq => {
+            val con2 = con.addNode(eq.l).addNode(eq.r).updateLazy
+            con2.explain(eq.l,eq.r) match {
+              case Some(path) => {
+                
+                
+                
+                path.toProof match {
+                  case Some(proof) => {
+                    val newSize = proof.root.conclusion.ant.size
+                    val oldSize = leftEqs.size
+                    
+  //                  println(newSize == proof.root.conclusion.ant.size)
+                    
+                    val line = oldSize + ", " + newSize + ", " + theorylemma + "\n"
+                    output.write(line)
+  //                  val fixedProof = Proof(fixedNode)
+  //                    if (proof.size > fixedProof.size) {
+  //                      val oldProof = Proof(fixedNode)
+  //                      println("increasing proof size, length:" + measure(oldProof)("length") + " vs " + measure(proof)("length") +  " trans length:" + measure(oldProof)("transLength") + " vs " + measure(proof)("transLength") + "\n" + Proof(fixedNode) + "\nto\n"+proof)
+  //                    }
+  //                    println(proof.root.conclusion.ant.size == path.originalEqs.size)
+  //                    if (fixedNode.conclusion.size < proof.root.conclusion.size) println("made it bigger: " + fixedNode.conclusion.size +" vs " + proof.root.conclusion.size)
+  //                    if (fixedNode.conclusion.size == proof.root.conclusion.size) println("stayed the same")
+  //                    if (fixedNode.conclusion.size > proof.root.conclusion.size) println("got smaller: " + fixedNode.conclusion.size +" vs " + proof.root.conclusion.size)
+                    comp = comp + (fixedNode.conclusion.size - proof.root.conclusion.size)
+  //                    println("compressing")
+                    proof.root
+                  }
+                  case None => fixedNode
+                }
+              }
+              case _ => fixedNode
+            }
+          })
+          
+          val x = if (eqToMap.isEmpty) {
+  //            println("reach this")
+            fixedNode 
+          }
+          else eqToMap.minBy(_.conclusion.size)
+  //          println(eqToMap.size + " ~ " + x)
+          x
         }
-        else eqToMap.minBy(_.conclusion.size)
-//          println(eqToMap.size + " ~ " + x)
-        x
+  //      if (resNode.conclusion.suc.size > 1) println("size > 1 in compressor!:\n " + Proof(resNode))
+        (resNode,theorylemma)
       }
-//      if (resNode.conclusion.suc.size > 1) println("size > 1 in compressor!:\n " + Proof(resNode))
-      (resNode,theorylemma)
     }
     
     val newProof = (proof foldDown traversal)._1
@@ -106,17 +106,17 @@ abstract class CongruenceCompressorNew extends (Proof[N] => Proof[N]) with fixNo
 //    println(newProof)
     
     // Resolve against axioms
-    val resProof = newProof.conclusion.suc.foldLeft(newProof)({(A,B) => 
-      eqNodesLeft.get(EqW(B)) match { //probably slow
-        case Some(node) => {
-//          println("blabla")
-          R(A,node,B)
-        }
-        case None => {
-          A
-        }
-      }
-    })
+//    val resProof = newProof.conclusion.suc.foldLeft(newProof)({(A,B) => 
+//      eqNodesLeft.get(EqW(B)) match { //probably slow
+//        case Some(node) => {
+////          println("blabla")
+//          R(A,node,B)
+//        }
+//        case None => {
+//          A
+//        }
+//      }
+//    })
     val resProof2 = newProof.conclusion.ant.foldLeft(newProof)({(A,B) => 
       reflMap.get(B) match {
         case Some(node) => R(A,node)
