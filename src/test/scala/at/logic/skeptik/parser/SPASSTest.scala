c62561e7199ac1f52f0950b0e0128258e68d565d
jgorzny <j.gorzny@gmail.com>
2014-08-13 18:29:50 -0700

    Add SPASS parser test case files

--- src/test/scala/at/logic/skeptik/parser/SPASSTest.scala
+++ src/test/scala/at/logic/skeptik/parser/SPASSTest.scala
@@ -3,14 +3,19 @@ package at.logic.skeptik.parser
 object SPASSTest {
   def main(args: Array[String]):Unit = {
 
+    println("1")
     val proof = ProofParserSPASS.read("examples/proofs/SPASS/completespaceforn_5.spass")
-    println(proof)
+    println(proof.toString)
+    
+    println("2")
     val proof2 = ProofParserSPASS.read("examples/proofs/SPASS/FSTP2.spass")
     println(proof2)
     
+    println("3")
     val proof3 = ProofParserSPASS.read("examples/proofs/SPASS/example2.spass")
     println(proof3)
     
+    println("4")
     val proof4 = ProofParserSPASS.read("examples/proofs/SPASS/example1.spass")
     println(proof4)
   }
