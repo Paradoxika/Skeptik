fc02e3e4ff38ef73a01614034fbfeaafd1e58cfb
jgorzny <j.gorzny@gmail.com>
2014-08-14 01:21:16 -0700

    Fix various test files
    All test files are now JUnit tests

--- src/test/scala/at/logic/skeptik/algorithm/compressor/FORPITest.scala
+++ src/test/scala/at/logic/skeptik/algorithm/compressor/FORPITest.scala
@@ -2,69 +2,81 @@ package at.logic.skeptik.algorithm.compressor
 
 import at.logic.skeptik.parser.ProofParserSPASS
 
-object FORPITest {
-  def main(args: Array[String]): Unit = {
+import org.junit.runner.RunWith
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.runner.JUnitRunner
 
-    println("1")
-    //Should  change the proof
-    val proof = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1.spass")
-    println(proof)
-    val result = FORecyclePivotsWithIntersection(proof)
-    println(result)
+@RunWith(classOf[JUnitRunner])
+class FORPILUSpecification extends SpecificationWithJUnit {
 
-    println("2")
-    //Bruno's -- should change
-    val proofb = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1b.spass")
-    println(proofb)
-    val resultb = FORecyclePivots(proofb)
-    println(resultb)
+  val proofa = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1.spass")
+  val resulta = FORecyclePivotsWithIntersection(proofa).toString
+  val expecteda = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1.result").mkString
 
-    println("3")
-    //Should not change the proof
-    val proofc = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1c.spass")
-    println(proofc)
-    val resultc = FORecyclePivots(proofc)
-    println(resultc)
+  //Bruno's -- should change
+  val proofb = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1b.spass")
+  val resultb = FORecyclePivots(proofb).toString
+  val expectedb = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1b.result").mkString
 
-    println("4")
-    //Should change the proof
-    val proofd = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1d.spass")
-    println(proofd)
-    val resultd = FORecyclePivots(proofd)
-    println(resultd)
+  //Should not change the proof
+  val proofc = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1c.spass")
+  val resultc = FORecyclePivots(proofc).toString
+  val expectedc = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1c.result").mkString
 
-    println("5")
-    //Should change the proof
-    val proofe = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1e.spass")
-    println(proofe)
-    val resulte = FORecyclePivots(proofe)
-    println(resulte)
+  //Should change the proof
+  val proofd = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1d.spass")
+  val resultd = FORecyclePivots(proofd).toString
+  val expectedd = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1d.result").mkString
 
-    println("6")
-    //Should not change the proof
-    val prooff = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1f.spass")
-    println(prooff)
-    val resultf = FORecyclePivots(prooff)
-    println(resultf)
+  //Should change the proof
+  val proofe = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1e.spass")
+  val resulte = FORecyclePivots(proofe).toString
+  val expectede = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1e.result").mkString
 
-    println("7")
-    //should not change
-    val proofg = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1g.spass")
-    println(proofg)
-    val resultg = FORecyclePivots(proofg)
-    println(resultg)
-    
-    println("8")
-    //Should not change the proof -- MRR test
-    val proofh = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1h.spass")
-    println(proofh)
-    val resulth = FORecyclePivots(proofh)
-    println(resulth) 
-    
-    
-    //TODO: add another example that tests intersection where a pivot is shared
-    
-    //TODO: add another example that tests intersection where a pivot is not shared (but similar)
-    
+  //Should not change the proof
+  val prooff = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1f.spass")
+  val resultf = FORecyclePivots(prooff).toString
+  val expectedf = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1f.result").mkString
+
+  //should not change
+  val proofg = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1g.spass")
+  val resultg = FORecyclePivots(proofg).toString
+  val expectedg = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1g.result").mkString
+
+  //Should not change the proof -- MRR test
+  val proofh = ProofParserSPASS.read("examples/proofs/SPASS/FORPIexample1h.spass")
+  val resulth = FORecyclePivots(proofh).toString
+  val expectedh = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/FORPILU/FORPIexample1h.result").mkString
+
+  //TODO: add another example that tests intersection where a pivot is shared
+
+  //TODO: add another example that tests intersection where a pivot is not shared (but similar)    
+
+  //TODO: give these meaninful names
+  "FORPILU" should {
+    "Compress the proof correctly (small compression)" in {
+      resulta.trim must beEqualTo(expecteda.trim)
+    }
+    "Parse the proof correctly" in {
+      resultb.trim must beEqualTo(expectedb.trim)
+    }
+    "Parse the proof correctly" in {
+      resultc.trim must beEqualTo(expectedc.trim)
+    }
+    "Parse the proof correctly" in {
+      resultd.trim must beEqualTo(expectedd.trim)
+    }
+    "Parse the proof correctly" in {
+      resulte.trim must beEqualTo(expectede.trim)
+    }
+    "Parse the proof correctly" in {
+      resultf.trim must beEqualTo(expectedf.trim)
+    }
+    "Parse the proof correctly" in {
+      resultg.trim must beEqualTo(expectedg.trim)
+    }
+    "Parse the proof correctly" in {
+      resulth.trim must beEqualTo(expectedh.trim)
+    }
   }
 }
--- src/test/scala/at/logic/skeptik/parser/SPASSTest.scala
+++ src/test/scala/at/logic/skeptik/parser/SPASSTest.scala
@@ -1,22 +1,40 @@
 package at.logic.skeptik.parser
 
-object SPASSTest {
-  def main(args: Array[String]):Unit = {
+import org.junit.runner.RunWith
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.runner.JUnitRunner
 
-    println("1")
-    val proof = ProofParserSPASS.read("examples/proofs/SPASS/completespaceforn_5.spass")
-    println(proof.toString)
-    
-    println("2")
-    val proof2 = ProofParserSPASS.read("examples/proofs/SPASS/FSTP2.spass")
-    println(proof2)
-    
-    println("3")
-    val proof3 = ProofParserSPASS.read("examples/proofs/SPASS/example2.spass")
-    println(proof3)
-    
-    println("4")
-    val proof4 = ProofParserSPASS.read("examples/proofs/SPASS/example1.spass")
-    println(proof4)
+@RunWith(classOf[JUnitRunner])
+class ProofParserSPASSSpecification extends SpecificationWithJUnit {
+
+  val proof1 = ProofParserSPASS.read("examples/proofs/SPASS/completespaceforn_5.spass")
+  val computed1 = proof1.toString
+  val expected1 = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/ProofParserSPASS/completespaceforn_5.result").mkString
+  
+  val proof2 = ProofParserSPASS.read("examples/proofs/SPASS/FSTP2.spass")
+  val computed2 = proof2.toString
+  val expected2 = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/ProofParserSPASS/FSTP2.result").mkString
+
+  val proof3 = ProofParserSPASS.read("examples/proofs/SPASS/example2.spass")
+  val computed3 = proof3.toString
+  val expected3 = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/ProofParserSPASS/example2.result").mkString
+
+  val proof4 = ProofParserSPASS.read("examples/proofs/SPASS/example1.spass")
+  val computed4 = proof4.toString
+  val expected4 = scala.io.Source.fromFile("examples/proofs/SPASS/testresults/ProofParserSPASS/example1.result").mkString
+
+  "ProofParserSPASS" should {
+    "Parse the first proof correctly" in {
+      computed1.trim must beEqualTo(expected1.trim)
+    }
+    "Parse the second proof correctly" in {
+      computed2.trim must beEqualTo(expected2.trim)
+    }
+    "Parse the third proof correctly" in {
+      computed3.trim must beEqualTo(expected3.trim)
+    }
+    "Parse the fourth proof correctly" in {
+      computed4.trim must beEqualTo(expected4.trim)
+    }
   }
 }
