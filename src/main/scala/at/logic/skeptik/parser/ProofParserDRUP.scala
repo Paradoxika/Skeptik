d087da0090215136586f8985766276dfeef7cc3c
Bruno <bruno.wp@gmail.com>
2015-01-09 00:49:08 +0300

    Make DRUP parser use the new parser for ordered TraceCheck proofs

--- src/main/scala/at/logic/skeptik/parser/ProofParserDRUP.scala
+++ src/main/scala/at/logic/skeptik/parser/ProofParserDRUP.scala
@@ -14,24 +14,25 @@ object ProofParserDRUP extends ProofParser[N] {
   def read(filename: String) : Proof[N] = {
     val filenameSplit = filename.split('.')
     val extension = filenameSplit.last
-    val name = (filenameSplit.dropRight(1) :\ "")(_ + _)
+    val name = filenameSplit.dropRight(1).mkString(".")
     
-    // println("obtaining lemmas")
+    println("obtaining lemmas")
     "./lib/drat-trim/drat-trim " + 
     name + ".cnf " + filename + " -l " + name + ".lemmas.drup" !
     
-    // println("converting to tracecheck")
+    println("converting to tracecheck")
     "./lib/drat-trim/drat-trim " + 
     name + ".cnf " + name + ".lemmas.drup -f -r " + name + ".temp.tc" !
     
-    // println("sorting tracechek")
-    Process("sort " + name + ".temp.tc -n") #>> new File(name + ".tc") !
+    println("sorting tracecheck")
+    Process("sort " + name + ".temp.tc -n") #>> new File(name + ".tco") !
     
-    // println("deleting temporary files")
+    println("deleting temporary files")
     "rm " + name + ".lemmas.drup" ! ;
     "rm " + name + ".temp.tc" ! ;
     
-    // println("reading tracecheck")
-    ProofParserTraceCheckOrdered.read(name + ".tc")
+    println("reading tracecheck file")
+    println(name)
+    ProofParserTraceCheckOrdered.read(name + ".tco")
   }
 }
