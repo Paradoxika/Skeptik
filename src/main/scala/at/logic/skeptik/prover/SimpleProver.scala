5f01bf1999098726bec5a777e0f43afcaf49be2f
Bruno <bruno.wp@gmail.com>
2015-03-18 11:41:08 +0100

    Fix simple prover

--- src/main/scala/at/logic/skeptik/prover/SimpleProver.scala
+++ src/main/scala/at/logic/skeptik/prover/SimpleProver.scala
@@ -9,30 +9,117 @@ import reflect.ClassTag
 
 // ToDo: (B) Use futures and Map from (goal, inference) to future to create DAG-proof!
 class SimpleProver[J <: Judgment, P <: ProofNode[J,P]: ClassTag](calculus: Calculus[J,P]) {
-  def prove(goal:J, timeout: Long = Long.MaxValue) : Option[P] = {
-    val deadline = System.nanoTime + timeout * 1000000 
-    
+  def prove(goal:J, timeout: Long = Long.MaxValue, maxheight: Int = Int.MaxValue) : Option[P] = {
+    val deadline = System.nanoTime + timeout * 1000000
+
     def proveRec(j: J, seen: Set[J])(implicit d:Int): Option[P] = {
-      if (System.nanoTime > deadline || (seen contains j) || j.logicalSize > goal.logicalSize) { // avoids cycles
-        debug(j); debug("seen subgoals below"); seen.map(debug _); debug("seen goal!"); debug("")
+      //debug(j)
+      
+      if (System.nanoTime > deadline) {
+        //debug("timeout")
         return None
       } 
-      else {
-        val proofs = for (rule <- calculus; subGoals <- rule(j)) yield {         
-          debug(j); debug("subgoals below"); seen.toList.reverse.map(debug _); debug(rule); subGoals.map(debug _); debug("")
-          val premises = subGoals.map({subGoal => proveRec(subGoal, seen + j)(d+1)})
-          debug("")
-          if (!premises.contains(None)) {
-            val proof = rule(premises.map(_.get).seq, j)
-            debug(proof); debug("")
-            proof
-          }
-          else None
-        }
-
-        argMin(proofs.filter(_ != None).map(_.asInstanceOf[Some[P]].get).toList, 
-               (p: P) => Proof(p).size)
+      else if (seen contains j) {
+        //debug("cycle")
+        return None
+      } 
+      else if (j.logicalSize > goal.logicalSize) {
+        //debug("goal size excess")
+        return None
+      } 
+      else if (d > maxheight) { // avoids cycles and limits height
+        //debug("max height"); 
+        return None
       }
+      else {
+        //debug("ok")
+        //debug("subgoal: " + j)
+        
+        
+        var bestProof: Option[P] = None
+        
+        val bestProofs = for (rule <- calculus) yield {
+          //debug("trying rule: " + rule)
+          
+          val subGoalsSeq = rule(j)
+          //debug("generated alternative subgoals: ")
+          //subGoalsSeq map {debug _}
+          //debug("")
+          val premisesSeq = for (subGoals <- subGoalsSeq) yield {
+            subGoals map {subGoal => proveRec(subGoal, seen + j)(d+1)}
+          }
+          //debug("premisesSeq: " + premisesSeq)
+          //debug("")
+          val filteredPremisesSeq = premisesSeq filter { premises => ! (premises contains None) }
+          //debug("filteredPremisesSeq: " + filteredPremisesSeq)
+          //debug("")
+          
+          val proofSeq = filteredPremisesSeq map { premises => rule(premises.map(_.get).seq, j)}
+          //debug("proofSeq: " + proofSeq)
+          //debug("")
+          var bestProofForRule: Option[P] = None
+          var bestProofForRuleSize = Int.MaxValue
+          for (pOption <- proofSeq) {
+            //debug(pOption)
+            
+            pOption match { 
+              case Some(p) => {
+                val pSize = Proof(p).size
+                if (pSize < bestProofForRuleSize) {
+                  bestProofForRuleSize = pSize
+                  bestProofForRule = pOption
+                }
+              }
+              case None => 
+            }
+          }
+          
+          //debug("bestProofForRule: " + bestProofForRule)
+          //debug("")
+          bestProofForRule
+        }
+ 
+        var bestProofSize = Int.MaxValue
+        for (pOption <- bestProofs) {
+          pOption match {
+              case Some(p) => {
+                val pSize = Proof(p).size
+                if (pSize < bestProofSize) {
+                  bestProofSize = pSize
+                  bestProof = pOption
+                }
+              }
+              case None => 
+          }
+        }
+        //debug("bestProof: " + bestProof)
+        //debug("")
+        
+        bestProof
+      }
+//      else {
+//        val proofs = for (rule <- calculus; subGoals <- rule(j)) yield {
+//          debug(j); 
+//          debug("subgoals below"); seen.toList.reverse.map(debug _); 
+//          debug(rule); subGoals.map(debug _); 
+//          debug("")
+//          val premises = subGoals map {subGoal => proveRec(subGoal, seen + j)(d+1)} 
+//          debug("")
+//          if (!premises.contains(None)) {
+//            val proof = rule(premises.map(_.get).seq, j)
+//            debug(proof); 
+//            debug("")
+//            proof
+//          }
+//          else None
+//        }
+//
+//        debug(j)
+//        debug(proofs)
+//        
+//        argMin(proofs.filter(_ != None).map(_.asInstanceOf[Some[P]].get).toList,
+//               (p: P) => Proof(p).size)
+//      }
     }
     proveRec(goal, Set())(0)
   }
