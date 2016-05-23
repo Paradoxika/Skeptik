35c537aa99cb76134e9172ae8b4bddd9ee6af0a5
Ezequiel Postan <ezequiel_postan@hotmail.com>
2016-05-23 00:01:30 -0300

    Reorganizing TPTP parsers structure

--- src/main/scala/at/logic/skeptik/parser/ParserTPTP.scala
+++ src/main/scala/at/logic/skeptik/parser/ParserTPTP.scala
@@ -2,43 +2,94 @@ package at.logic.skeptik.parser
 
 import scala.util.parsing.combinator._
 import collection.mutable.{HashMap => MMap}
-import java.io.FileReader
 import at.logic.skeptik.proof.Proof
 import at.logic.skeptik.proof.sequent.{SequentProofNode => Node}
 import at.logic.skeptik.proof.sequent.lk.{R, Axiom, UncheckedInference}
-import at.logic.skeptik.expression.formula._
 import at.logic.skeptik.expression._
 import at.logic.skeptik.judgment.immutable.{SeqSequent => Sequent}
 
-object ParserTPTP extends ProofCombinatorParser[Node] with ParserTPTP
+/**
+  * This modules describe the TPTP syntax as descrobed in
+  * www.cs.miami.edu/~tptp/TPTP/SyntaxBNF.html
+  *
+  * The non terminals don't follow camelcase convention to
+  * reflect the grammar in a more natural way.
+  *
+  * @author Ezequiel Postan
+  * @since 23.05.2016
+  */
+
+class UnexpectedEmptyTPTPFileException extends Exception("Unexpected Empty File")
+
+object ProofParserTPTP   extends ProofCombinatorParser[Node] with ProofParserTPTP
+object ProblemParserTPTP extends ProblemParserTPTP
+
+/**
+  * The BaseParserTPTP implements the common parsers shared both
+  * by problems and proof objects described by the TPTP syntax
+  */
+trait BaseParserTPTP
+extends JavaTokenParsers with PackratParsers {
+
+  def fof_annotated = "fof" ~ "(" ~ name ~ "," ~ formula_role ~ "," ~ fof_logic_formula ~ opt(annotations) ~ ")" <~ "."
+  def cnf_annotated = "cnf" ~ "(" ~ name ~ "," ~ formula_role ~ "," ~ cnf_logic_formula ~ opt(annotations) ~ ")" <~ "."
+  def tff_annotated = "tff" ~ "(" ~ name ~ "," ~ formula_role ~ "," ~ tff_logic_formula ~ opt(annotations) ~ ")" <~ "."
+  def thf_annotated = "thf" ~ "(" ~ name ~ "," ~ formula_role ~ "," ~ thf_logic_formula ~ opt(annotations) ~ ")" <~ "."
+  def tpi_annotated = "tpi" ~ "(" ~ name ~ "," ~ formula_role ~ "," ~ tpi_logic_formula ~ opt(annotations) ~ ")" <~ "."
+  def tpi_logic_formula = fof_logic_formula
 
 
-trait ParserTPTP
-extends JavaTokenParsers with RegexParsers {
-  
+  def fof_logic_formula = ???
+  def cnf_logic_formula = ???
+  def tff_logic_formula = ???
+  def thf_logic_formula = ???
+
+  def annotations  : PackratParser[String] = ???
+
+  def name         : PackratParser[String] = atomic_word | wholeNumber
+  def formula_role : PackratParser[String] = "axiom" | "hypothesis" | "definition" | "assumption" |
+                                             "lemma" | "theorem" | "corollary" | "conjecture" |
+                                             "negated_conjecture" | "plain" | "type" | "fi_domain" |
+                                             "fi_functors" | "fi_predicates" | "unknown"
+
+  def atomic_word   : PackratParser[String] = lower_word | single_quoted
+  def lower_word    : PackratParser[String] = regex("[a-z][a-zA-Z0-9_]*".r)
+  def single_quoted : PackratParser[String] = """'[^']*'""".r ^^ { _.drop(1).dropRight(1) }
+
+  def comments = ???
+
+}
+
+trait ProblemParserTPTP
+extends BaseParserTPTP {
+}
+
+trait ProofParserTPTP
+extends BaseParserTPTP {
+
+  private var varMap   = new MMap[String,E]
+  private var exprMap  = new MMap[String,E]
   private var proofMap = new MMap[String,Node]
-  private var exprMap = new MMap[String,E]
-  private var varMap = new MMap[String,E]
 
-  def reset(): Unit = ()
-
-  //Note (jgorzny; 27 Apr 2016):
-  //I don't think this parser was ever completed or used (in whatever state it exists in)
-  //Use *only* after testing; the TODOs below suggest some work needs to be done.
-  
-  //TODO: remove debugging lines
-  //TODO: actually store the information; right now it parses the stuff and throws it away
-  
-  //returns the actual proof
- def proof: Parser[Proof[Node]] = rep(cnf) ^^ {   
- 	case list =>{ 
- 		val p = Proof((list.last).last)
- 		exprMap = new MMap[String,E]
- 		p
-    }
+  def reset() : Unit = {
+    varMap.clear()
+    exprMap.clear()
+    proofMap.clear()
   }
-  
-  
+
+  //returns the actual proof
+  def proof: Parser[Proof[Node]] = TPTP_file ^^ {p => if (p.nonEmpty) p.last
+                                                      else throw new UnexpectedEmptyTPTPFileException
+                                                }
+
+  def TPTP_file  : PackratParser[List[Proof[Node]]] = rep(TPTP_input)
+  def TPTP_input : PackratParser[Proof[Node]] = annotated_formula | include
+
+  def include  = ???
+
+  def annotated_formula : PackratParser[Proof[Node]] = ???
+
+  /*
   def cnfName: Parser[String] = name ^^ {
     case _ => {
       println("sos")
@@ -131,4 +182,5 @@ extends JavaTokenParsers with RegexParsers {
         s
       }
     }
+  */
 }
--- src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
+++ src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
@@ -2,7 +2,7 @@ package at.logic.skeptik.parser
 
 object TPTPTest {
   def main(args: Array[String]):Unit = {
-    val proof = ParserTPTP.read("examples/proofs/SPASS/C(3_3)_attempted_instantiation.tptp")
+    val proof = ProofParserTPTP.read("examples/proofs/SPASS/C(3_3)_attempted_instantiation.tptp")
     println(proof)
   }
 }
