8638fd402b2bc538b87c8acb1dc72878d26376fb
Ezequiel Postan <ezequiel_postan@hotmail.com>
2016-05-26 19:31:32 -0300

    TPTP Tests correction

--- src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
+++ src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
@@ -1,8 +1,13 @@
 package at.logic.skeptik.parser
 
+import at.logic.skeptik.parser.TPTPParsers.{ProblemParserCNFTPTP, ProblemParserFOFTPTP}
+
 object TPTPTest {
   def main(args: Array[String]):Unit = {
-    val proof = ProofParserTPTP.read("examples/proofs/SPASS/C(3_3)_attempted_instantiation.tptp")
-    println(proof)
+    val problem1 = ProblemParserCNFTPTP.problem("examples/problems/CNF/CNF.cnfp")
+    println(problem1)
+    println("\n\n\n")
+    val problem2 = ProblemParserFOFTPTP.problem("examples/problems/FOF/FOF.fofp")
+    println(problem2)
   }
 }
