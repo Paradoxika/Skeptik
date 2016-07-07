cea11e44c1c413163f623bf3bf1faadaa4d220f1
itegulov <ditegulov@gmail.com>
2016-07-07 17:54:02 +0300

    Add AGT001+1 example from TPTP

--- src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
+++ src/test/scala/at/logic/skeptik/parser/TPTPTest.scala
@@ -1,19 +1,11 @@
 package at.logic.skeptik.parser
 
-import at.logic.skeptik.parser.TPTPParsers.{ProblemParserCNFTPTP, ProblemParserFOFTPTP, ProofParserCNFTPTP}
+import at.logic.skeptik.parser.TPTPParsers.ProblemParserFOFTPTP
 
 object TPTPTest {
   def main(args: Array[String]):Unit = {
-    val problem1 = ProblemParserCNFTPTP.problem("examples/problems/CNF/CNF.cnfp")
+    val problem1 = ProblemParserFOFTPTP.problem("examples/problems/FOF/AGT001+1.fofp")
     println(problem1)
-    ProblemParserCNFTPTP.resetVarsSeen()
-    println("\n\n\n")
-    val problem2 = ProblemParserFOFTPTP.problem("examples/problems/FOF/FOF.fofp")
-    println(problem2)
     ProblemParserFOFTPTP.resetVarsSeen()
-    println("\n\n\n")
-    val proof1 = ProofParserCNFTPTP.read("examples/proofs/TPTP/complexCNF.tptp")
-    println(proof1)
-    println(ProofParserCNFTPTP.getVariables)
   }
 }
