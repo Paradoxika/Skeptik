f19d95af27bd64b0aeedee5a9b68e17fbe6c3904
Daniyar Itegulov <ditegulov@gmail.com>
2016-07-28 17:46:19 +0500

    Change Literal's toString

--- src/main/scala/at/logic/skeptik/algorithm/prover/structure/immutable/Literal.scala
+++ src/main/scala/at/logic/skeptik/algorithm/prover/structure/immutable/Literal.scala
@@ -11,4 +11,6 @@ case class Literal(unit: E, negated: Boolean) {
   def unary_! = Literal(unit, !negated)
 
   def toClause: Clause = if (negated) new SetSequent(Set(unit), Set.empty) else new SetSequent(Set.empty, Set(unit))
+
+  override def toString: String = if (negated) s"$unit ⊢" else s"⊢ $unit"
 }
