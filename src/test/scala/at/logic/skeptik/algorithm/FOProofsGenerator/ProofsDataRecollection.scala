610d2c35fafec6373349b1fa85198fe1fcfd1136
Ezequiel Postan <ezequiel_postan@hotmail.com>
2016-07-31 20:09:13 -0300

    Data Recollection

--- src/test/scala/at/logic/skeptik/algorithm/FOProofsGenerator/ProofsDataRecollection.scala
+++ src/test/scala/at/logic/skeptik/algorithm/FOProofsGenerator/ProofsDataRecollection.scala
@@ -15,14 +15,158 @@ import at.logic.skeptik.proof.sequent.{SequentProofNode => Node}
 import collection.mutable.{HashSet, Set => MSet}
 import scala.io.Source
 
+
 /**
-  * Here we will explore the parameters of real proofs such as
-  * number of variables, constants, predicates, functions, proof's height
-  * and proof's size.
+  * The object RandomProofsTest show results related to proofs randombly generated
+  */
+object RandomProofsTest {
+
+  def generateProof(proofHeight : Int) : (Proof[Node],MSet[Var]) = {
+    val generator = new ProofGenerator(proofHeight)
+    try {
+      (generator.generateProof(),generator.getVariables())
+    } catch {
+      case e : Exception =>
+        generateProof(proofHeight)
+    }
+  }
+
+  import ProofsDataRecollection._
+
+  def main(args : Array[String]) : Unit = {
+
+    val report = new PrintWriter("proofsData.log")
+    report.println("Size,Height,Axioms,Contractions,Resolutions,Predicates,Functions,Constants,Variables")
+    report.flush()
+
+    val numberOfProofs : Int = 68
+    val data : Array[ProofData] = new Array[ProofData](numberOfProofs)
+
+    var next = 0
+    for (i <- 1 to numberOfProofs) {
+      val (proof,vars) : (Proof[Node],MSet[Var]) = generateProof(3)
+      val proofData    : ProofData               = numberOf(proof,vars)
+      proofData.setHeight(proofHeight(proof))
+      report.println(format(proofData.size)+ ", " + format(proofData.height) + ", " + proofData.toString)
+      report.flush()
+      data(next) = proofData
+      next += 1
+    }
+
+    val heights      : Array[Double] = new Array[Double](numberOfProofs)
+    val axioms       : Array[Double] = new Array[Double](numberOfProofs)
+    val resolutions  : Array[Double] = new Array[Double](numberOfProofs)
+    val contractions : Array[Double] = new Array[Double](numberOfProofs)
+    for(i <- 0 until numberOfProofs){
+      heights(i)      = (data(i).height * 1.0) / data(i).size
+      axioms(i)       = (data(i).numberOfAxioms * 1.0)/data(i).size
+      resolutions(i)  = (data(i).numberOfResolutions * 1.0)/data(i).size
+      contractions(i) = (data(i).numberOfContractions * 1.0)/data(i).size
+    }
+
+    var averageSize         = 0.0
+    var averageHeight       = 0.0
+    var averageAxioms       = 0.0
+    var averageResolutions  = 0.0
+    var averageContractions = 0.0
+    for(i <- 0 until numberOfProofs) {
+      println(data(i).size + " , " /*+ heights(i) + " , "*/ + axioms(i) + " , " + contractions(i) + " , " + resolutions(i))
+      averageSize         += data(i).size
+      averageHeight       += heights(i)
+      averageAxioms       += axioms(i)
+      averageResolutions  += resolutions(i)
+      averageContractions += contractions(i)
+    }
+    averageSize         /= numberOfProofs
+    averageHeight       /= numberOfProofs
+    averageAxioms       /= numberOfProofs
+    averageResolutions  /= numberOfProofs
+    averageContractions /= numberOfProofs
+    println("Avarages")
+    println(averageSize + " , " + averageHeight + " , " + averageAxioms + " , " + averageContractions + " , " + averageResolutions)
+  }
+}
+
+/**
+  * The object FilesTests show results related to real proofs collected from SPASS
+  */
+
+object FilesTests {
+
+  import ProofsDataRecollection._
+
+  def main(args : Array[String]) : Unit = {
+    val path      : String = "/home/eze/Escritorio/Skeptik/GoodProofs/ALL/"
+    val proofList : String = "/home/eze/Escritorio/Skeptik/GoodProofs/ALL/list.txt"
+
+    val problemSetS = getProblems(proofList, path)
+
+    val report = new PrintWriter("proofsData.log")
+    report.println("Size,Height,Axioms,Contractions,Resolutions,Predicates,Functions,Constants,Variables")
+    report.flush()
+
+    val numberOfProofs : Int = 68
+    val data : Array[ProofData] = new Array[ProofData](numberOfProofs)
+
+    var totalCountT : Int = 0
+    var next = 0
+    for (probY <- problemSetS) {
+      totalCountT = totalCountT + 1
+      println("Proof " + totalCountT + ": " + probY)
+      val proof     : Proof[Node] = ProofParserSPASS.read(probY)
+      val vars      : MSet[Var]   = ProofParserSPASS.getVars()
+      val proofData : ProofData   = numberOf(proof,vars)
+      proofData.setHeight(proofHeight(proof))
+      report.println(format(proofData.size)+ ", " + format(proofData.height) + ", " + proofData.toString)
+      report.flush()
+      if(proofData.size >= 20) {
+        data(next) = proofData
+        next += 1
+      }
+    }
+
+    val heights      : Array[Double] = new Array[Double](numberOfProofs)
+    val axioms       : Array[Double] = new Array[Double](numberOfProofs)
+    val resolutions  : Array[Double] = new Array[Double](numberOfProofs)
+    val contractions : Array[Double] = new Array[Double](numberOfProofs)
+    for(i <- 0 until numberOfProofs){
+      heights(i)      = (data(i).height * 1.0) / data(i).size
+      axioms(i)       = (data(i).numberOfAxioms * 1.0)/data(i).size
+      resolutions(i)  = (data(i).numberOfResolutions * 1.0)/data(i).size
+      contractions(i) = (data(i).numberOfContractions * 1.0)/data(i).size
+    }
+
+    var averageSize         = 0.0
+    var averageHeight       = 0.0
+    var averageAxioms       = 0.0
+    var averageResolutions  = 0.0
+    var averageContractions = 0.0
+    for(i <- 0 until numberOfProofs) {
+      println(data(i).size + " , " /*+ heights(i) + " , "*/ + axioms(i) + " , " + contractions(i) + " , " + resolutions(i))
+      averageSize         += data(i).size
+      averageHeight       += heights(i)
+      averageAxioms       += axioms(i)
+      averageResolutions  += resolutions(i)
+      averageContractions += contractions(i)
+    }
+    averageSize         /= numberOfProofs
+    averageHeight       /= numberOfProofs
+    averageAxioms       /= numberOfProofs
+    averageResolutions  /= numberOfProofs
+    averageContractions /= numberOfProofs
+    println("Avarages")
+    println(averageSize + " , " + averageHeight + " , " + averageAxioms + " , " + averageContractions + " , " + averageResolutions)
+  }
+}
+
+/**
+  * Here we will implement methods to explore the parameters of proofs such as
+  * number of variables, constants, predicates, functions, proof's height and
+  * proof's size.
   */
 object ProofsDataRecollection {
 
-  private def format(x : Int) : String =
+  def format(x : Int) : String =
     if(x < 10) " " + x.toString
     else x.toString
 
@@ -37,27 +181,7 @@ object ProofsDataRecollection {
     outTj
   }
 
-  def main(args : Array[String]) : Unit = {
-    val path      : String = "/home/eze/Escritorio/Skeptik/GoodProofs/ALL/"
-    val proofList : String = "/home/eze/Escritorio/Skeptik/GoodProofs/ALL/list.txt"
 
-    val problemSetS = getProblems(proofList, path)
-
-    val report = new PrintWriter("proofsData.log")
-    report.println("Size,Height,Axioms,Contractions,Resolutions,Predicates,Functions,Constants,Variables")
-    report.flush()
-
-    var totalCountT : Int = 0
-    for (probY <- problemSetS) {
-      totalCountT = totalCountT + 1
-      println("Proof " + totalCountT + ": " + probY)
-      val proof     : Proof[Node] = ProofParserSPASS.read(probY)
-      val vars      : MSet[Var]   = ProofParserSPASS.getVars()
-      val proofData : ProofData   = numberOf(proof,vars)
-      report.println(format(proof.size)+ ", " + format(proofHeight(proof)) + ", " + proofData.toString)
-      report.flush()
-    }
-  }
 
 
   def proofHeight(p : Proof[Node]) : Int = {
@@ -135,6 +259,12 @@ object ProofsDataRecollection {
 class ProofData(val numberOfAxioms : Int, val numberOfContractions : Int, val numberOfResolutions : Int,
                 val numberOfPredicates : Int, val numberOfFunctions : Int, val numberOfConstants : Int,
                 val numberOfVariables : Int) {
+
+  val size = numberOfResolutions + numberOfContractions + numberOfAxioms
+  private var proofHeight : Int = 0
+  def setHeight(h : Int) : Unit =
+    proofHeight = h
+  def height : Int = proofHeight
 
   private def format(x : Int) : String =
     if(x < 10) " " + x.toString
